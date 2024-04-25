51c8be87a2b3e6a48ce8b01981c739ffddff931c
Amara <fray.amara@gmail.com>
2024-04-25 12:47:28 +0530

    Update process.c

--- process.c
+++ process.c
@@ -3,14 +3,9 @@
 #include <unistd.h>
 int main()
 {
-
-	// make two process which run same
-	// program after this instruction
 	pid_t p = fork();
 	if(p<0){
-	perror("fork fail");
-	exit(1);
+		exit(1);
 	}
-	printf("Hello world!, process_id(pid) = %d \n",getpid());
 	return 0;
 }
