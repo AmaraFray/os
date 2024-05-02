4d381c1289e5665c0ffbc580ce0305e5e1bf29d0
Amara <fray.amara@gmail.com>
2024-05-02 12:58:44 +0530

    Create mutex.c

---
+++ mutex.c
@@ -0,0 +1,1 @@
+//                                                                                            pthread_mutex_lock pthread_mutex_unlock pthread_create pthread_mutex_destroy pthread_mutex_init
